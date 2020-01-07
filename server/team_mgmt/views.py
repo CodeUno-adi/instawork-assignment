44112160185c437ee5227433111e5d818561e542
Aditya N <gojeta.aditya@gmail.com>
2020-01-08 02:32:28 +0530

    Code cleanup

--- server/team_mgmt/views.py
+++ server/team_mgmt/views.py
@@ -1,8 +1,4 @@
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
 from team_mgmt.serializers import TeamMemberSerializer
 from .models import TeamMember
 
